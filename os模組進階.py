dd6632fa9b07aca740082d930da05faf6e7cb841
Tsai-Yueh-Ting <60865476+Tsai-Yueh-Ting@users.noreply.github.com>
2020-02-21 13:01:37 +0800

    os模組進階

---
+++ os模組進階.py
@@ -0,0 +1,18 @@
+aaa=[]
+F=open('新文字文件.txt','r')
+lines=F.read()
+while lines:
+   aaa.append(lines)
+   lines=F.read()
+   print(aaa)
+     
+F.close()
+
+#J120449146
+#Csrcejo37
+
+#pijht880379
+#B610722610722
+#b610722610722
+#J120449146
+#j120449146
